59c6aad4cee1c6639ff329b231ae153d009a4322
Imam Setiawan <s.imam29@gmail.com>
2026-02-14 07:49:56 +0700

    return proper HTTP status

--- app/router/coffee.py
+++ app/router/coffee.py
@@ -1,15 +1,15 @@
 from app.schema.coffee import CoffeeResponse
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from app.utils.query_params import standard_params
 
 coffee_router = APIRouter(tags=["Coffee"])
 
-@coffee_router.get("/coffees")
+@coffee_router.get("/coffees", status_code=status.HTTP_200_OK)
 def get_coffees(params = Depends(standard_params)):
     return {"message": "Hello World", "params": params}
 
-@coffee_router.post("/coffees")
+@coffee_router.post("/coffees", status_code=status.HTTP_201_CREATED)
 def create_coffee():
     return CoffeeResponse(
         id="1",
--- app/router/tea.py
+++ app/router/tea.py
@@ -1,16 +1,16 @@
 from app.schema.tea import TeaResponse
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from app.utils.query_params import standard_params
 
 tea_router = APIRouter(tags=["Tea"])
 
-@tea_router.get("/teas")
+@tea_router.get("/teas", status_code=status.HTTP_200_OK)
 def get_teas(params = Depends(standard_params)):
     return {"message": "Hello World", "params": params}
 
 
-@tea_router.post("/teas")
+@tea_router.post("/teas", status_code=status.HTTP_201_CREATED)
 def create_tea():
     return TeaResponse(
         id="1",
