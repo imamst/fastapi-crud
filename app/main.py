4645e12371cffb3cb0f54a2161b200a6019671a2
Imam Setiawan <s.imam29@gmail.com>
2026-02-14 07:04:56 +0700

    add api docs using # uv add scalar-fastapi #

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 from app.core.settings import settings
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.VERSION
 )
+
+@app.get("/scalar")
+def get_scalar():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title=app.title
+    )
